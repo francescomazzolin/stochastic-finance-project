ae5e34ac14bc52bc6307a43cfc1d74dc132071e9
PacoCoding <154080365+PacoCoding@users.noreply.github.com>
2025-01-01 19:41:07 +0100

    Update analytical_functions.py
    Added credit spread model

--- analytical_functions.py
+++ analytical_functions.py
@@ -28,3 +28,22 @@ FUNCTION TO COMPUTE THE PROBABILY OF DEFAULT
 def risk_neutral_default_probability(V, B, r, sigma, T, t):
     d2 = (np.log(V / B) + (r - 0.5 * sigma**2) * (T - t)) / (sigma * np.sqrt(T - t))
     return norm.cdf(-d2)
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+FUNCTION TO COMPUTE THE CREDIT SPREAD
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def credit_spread_model(V, K, sigma, r, T, t):
+    riskless = np.exp(-r * (T - t))  # Riskless bond price
+
+    # Calculate d1 and d2
+    d1 = (np.log(V / K) + (r + 0.5 * sigma**2) * (T - t)) / (sigma * np.sqrt(T - t))
+    d2 = d1 - sigma * np.sqrt(T - t)
+
+    # Defaultable bond price using Merton's formula
+    defaultable_bond = V * norm.cdf(d1) - K * riskless * norm.cdf(d2)
+
+    # Credit spread calculation
+    credit_spread = -1 / (T - t) * np.log(defaultable_bond / (K * riskless))
+
+    return credit_spread
