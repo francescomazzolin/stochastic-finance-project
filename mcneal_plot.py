126d797fbd2ebb40e258cb6f7e3f6351f18eebab
PacoCoding <francescosalvagnin001@gmail.com>
2025-01-03 17:07:53 +0100

    modify plots

--- mcneal_plot.py
+++ mcneal_plot.py
@@ -1,39 +1,31 @@
-#%%
 import matplotlib.pyplot as plt
-import pandas as pd
-from scipy.stats import norm
 import numpy as np
 from analytical_functions import credit_spread_model
 
-#V= 3829968375.7
-#K = 931124000.0
-V=100
-K=60
-r = 0
-T= 2
-t = 0
-sigma = 0.25
+def plot_spread_vol(V, K, r, T, t, debug=False):
+    vol_interval = np.linspace(0.01, 0.5, 1000)
+    credit_spread_vol = [credit_spread_model(V, K, sigma, r, T, t) for sigma in vol_interval]
+    credit_spread_vol_percentage = [cs * 100 for cs in credit_spread_vol]
 
-def plot_spread_vol(V,K,r,T,t):
-    vol_interval = np.linspace(0.01,0.5,1000)
-    credit_spread_vol = [credit_spread_model(V,K,sigma,r,T,t) for sigma in vol_interval]
-
-    plt.figure(figsize=(12,6))
-    plt.plot(vol_interval,credit_spread_vol,label = 'Credit Spread')
-    plt.xlabel('Volatility (sigma)')
-    plt.ylabel('Credit Spread (%)')
+    plt.figure(figsize=(12, 6))
+    plt.plot(vol_interval, credit_spread_vol_percentage, label='Credit Spread')
+    plt.xlabel('Volatility (σ)')
+    plt.ylabel('Credit Spread (%)')  
     plt.title('Impact of Volatility on Credit Spread')
     plt.grid(False)
+    plt.legend()
     plt.show()
 
-def plot_spread_time(V,K,r,T,t):
-    time_interval = np.linspace(0,5,1000)
-    credit_spread_time = [credit_spread_model(V,K,sigma,r,T,t) for T in time_interval]
+def plot_spread_time(V, K, r, T, t, sigma=0.25, debug=False):
+    time_interval = np.linspace(0.01, 5, 1000)  
+    credit_spread_time = [credit_spread_model(V, K, sigma, r, T, t) for T in time_interval]
+    credit_spread_time_percentage = [cs * 100 for cs in credit_spread_time]
 
-    plt.figure(figsize=(12,6))
-    plt.plot(time_interval,credit_spread_time,label ='Credit Spread')
-    plt.xlabel("Time to Maturity")
-    plt.ylabel("Credit Spread (%)")
+    plt.figure(figsize=(12, 6))
+    plt.plot(time_interval, credit_spread_time_percentage, label='Credit Spread')
+    plt.xlabel("Time to Maturity (T)")
+    plt.ylabel("Credit Spread (%)") 
     plt.title("Impact of Maturity on Credit Spread")
     plt.grid(False)
+    plt.legend()
     plt.show()
