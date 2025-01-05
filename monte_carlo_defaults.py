0cb0e0349475e29676c0dad05d03d9ae7cfedebd
PacoCoding <154080365+PacoCoding@users.noreply.github.com>
2025-01-05 17:16:19 +0100

    Update monte_carlo_defaults.py
    add function for calculating probability with jumps

--- monte_carlo_defaults.py
+++ monte_carlo_defaults.py
@@ -20,3 +20,36 @@ def monte_carlo_merton(V_0, K, r, sigma, T, M):
     
 
     return Equity_sim_mean, Debt_sim_mean, prob_default
+
+
+def merton_jumps_default(V0, K, T, M, N, lam, m, v, r, sigma):
+   
+    dt = T / N  
+    paths = np.zeros((M, N))  
+    paths[:, 0] = V0  
+
+    for t in range(1, N):
+        z = np.random.standard_normal(M)  
+        jump_counts = np.random.poisson(lam * dt, M)  
+        jump_sizes = np.random.normal(m, v, M) 
+        
+        
+        paths[:, t] = paths[:, t - 1] * np.exp(
+            (r - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * z
+        ) * np.exp(jump_counts * jump_sizes)
+
+   
+    V_T = paths[:, -1]  
+    S_T = np.maximum(V_T - K, 0)  
+    B_T = V_T - S_T  
+
+    
+    defaulted_paths = np.any(paths < K, axis=1)
+    print(f"Number of default paths: {np.sum(defaulted_paths)}")
+    prob_default = (np.sum(defaulted_paths) / M) * 100  
+
+    Equity_mean = np.mean(S_T)
+    Debt_mean = np.mean(B_T)
+
+    return Equity_mean, Debt_mean, prob_default
+
