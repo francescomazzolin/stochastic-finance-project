d0d5f40e0caea9b9befa45904679611ab8088acb
PacoCoding <francescosalvagnin001@gmail.com>
2025-01-14 11:03:54 +0100

    piccoli accorgimenti

--- stream.py
+++ stream.py
@@ -3,37 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from decimal import Decimal, getcontext
+from analytical_functions import default_probability
 
-# Function to compute default probability using Merton's model
-def default_probability(V, B, r, sigma, T, t, precision=20):
-    """
-    Computes the real-world probability of default according to Merton's model.
-    """
-    getcontext().prec = precision
-
-    if B != 0:
-        V_d = Decimal(float(V))
-        B_d = Decimal(float(B))
-        r_d = Decimal(float(r))
-        sigma_d = Decimal(float(sigma))
-        T_d = Decimal(T)
-        arg = (np.log(float(B_d / V_d)) - float(Decimal('0.5') * sigma_d**2)) / float(sigma_d * np.sqrt(T_d - t))
-        return float(norm.cdf(arg))
-    else:
-        return 0
-
-
-# Streamlit App
 st.title("Interactive Merton Model: Impact of Volatility on Probability of Default (PD)")
 st.write("""
-This interactive app shows how changing the **volatility** (σ) of the firm's asset impacts the **Probability of Default (PD)** 
+This plot represents how changing the **volatility** (σ) of the firm's asset impacts the **Probability of Default (PD)** 
 based on Merton's model. 
 Use the sliders and inputs to adjust the parameters.
 """)
 
 
-
-# Sidebar inputs for model parameters
 st.sidebar.header("Model Parameters")
 V = st.sidebar.number_input("Firm's Asset Value (V)", min_value=1.0, value=100.0, step=1.0)
 B = st.sidebar.number_input("Debt Due at Maturity (B)", min_value=1.0, value=80.0, step=1.0)
@@ -47,13 +26,12 @@ with st.sidebar:
     st.markdown("""*Made by: <br> Giada Martini, Francesco Mazzolin, Francesco Salvagnin, Nicolas Stecca*""", unsafe_allow_html=True)
 
 # Volatility Slider for Interaction
-sigma_selected = st.slider("Volatility (σ)", 0.01, 1.0, 0.2, 0.01)
-
-# Compute probability of default with selected volatility
+st.markdown("<h3 style='font-size:24px;'>Volatility (σ)</h3>", unsafe_allow_html=True)
+sigma_selected = st.slider("", 0.01, 1.0, 0.2, 0.01)  
 pd = default_probability(V, B, r, sigma_selected, T, t)
 st.subheader(f"Probability of Default (PD) for σ = {sigma_selected:.2f}: {f'{pd:.3%}'}")
 
-# Plot PD vs Volatility
+
 volatility_range = np.linspace(0.01, 1.0, 100)
 pd_values = [default_probability(V, B, r, sigma, T, t) for sigma in volatility_range]
 
